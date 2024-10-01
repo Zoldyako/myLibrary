72cb46ed2a7dbcffe386a80cd37e7f4273763341
José Victor <zoldyakopersonal@gmail.com>
2024-10-01 06:26:10 -0300

    Finished displayBook() function

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const tbody = document.querySelector('tbody');
+const newBook = document.querySelector('button');
 const myLibrary = [];
+
+
+let lordOfTheRings  = new Book('Lord of The Rings', 'J.R.R. Tolkien', 295, 'Reading');
+let lightingThief   = new Book('Percy Jackson and the Lighting Thief', 'Rick Riordan', 100, 'Read');
+
+myLibrary[0] = lordOfTheRings;
+myLibrary[1] = lightingThief;
 
 
 function Book(title, author, pages, read) {
@@ -13,33 +21,36 @@ Book.prototype.bookInfo = function () {
     return `${this.title} by author ${this.author}, has ${this.pages}, ${this.read}`
 }
 
+
 function addBookToLibrary() {}
 
 
 function displayBook() {
-    myLibrary.forEach((myLibrary) => {
+    
+    //  Iterates myLibrary array items
+    //  Note to my self: Good to name the element parameter in context
+    //  Here we are interating in an array of books so it is good to call the element 'book' :)
+    myLibrary.forEach(book => {
+
+        // Creates a 'tr' element for the current book
         let tr = document.createElement('tr');
-        let title = document.createElement('td'),
-            author = document.createElement('td'), 
-            pages = document.createElement('td'), 
-            read = document.createElement('td');
 
-        title.innerText = myLibrary.title;
-        author.innerText = myLibrary.author;
-        pages.innerText = myLibrary.pages;
-        read.innerText  = myLibrary.read;
+        // After recreating the "God I wished there was an easier way to do this" meme 
+        // by asigning each 'td' manually I asked ChatGPT if *there was a easier/cleaner way to do this* 
+        // well turns out I'm dummer than the AI and there was a better way to do this
+        // I'll use this method in the future (if I don't forget :P)
 
-        tr.append(title, author, pages, read);
+        ['title', 'author', 'pages', 'read'].forEach(key => {
+            let td = document.createElement('td');
+
+            td.innerText = book[key];
+            tr.appendChild(td);
+        });
+
         tbody.appendChild(tr);
     }); 
 }
 
-
-let lordOfTheRings  = new Book('Lord of The Rings', 'J.R.R. Tolkien', 295, 'Reading');
-let lightingThief   = new Book('Percy Jackson and the Lighting Thief', 'Rick Riordan', 100, 'Read');
-
-myLibrary[0] = lordOfTheRings;
-myLibrary[1] = lightingThief;
 
 
 displayBook();
