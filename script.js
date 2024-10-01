db3910761def28843b2a85f02ba59fa77daec912
José Victor <zoldyakopersonal@gmail.com>
2024-10-01 08:24:37 -0300

    Removed the commented lines

--- script.js
+++ script.js
@@ -3,13 +3,6 @@ const newBook = document.querySelector('button');
 const myLibrary = [];
 
 
-let lordOfTheRings  = new Book('Lord of The Rings', 'J.R.R. Tolkien', 295, 'Reading');
-let lightingThief   = new Book('Percy Jackson and the Lighting Thief', 'Rick Riordan', 100, 'Read');
-
-myLibrary[0] = lordOfTheRings;
-myLibrary[1] = lightingThief;
-
-
 function Book(title, author, pages, read) {
     this.title  = title,
     this.author = author,
@@ -22,23 +15,18 @@ Book.prototype.bookInfo = function () {
 }
 
 
-function addBookToLibrary() {}
+function addBookToLibrary(title, author, pages, read) {
+
+    myLibrary.push(new Book(title, author, pages, read));
+    displayBook();
+}
 
 
 function displayBook() {
+    tbody.innerHTML = '';
     
-    //  Iterates myLibrary array items
-    //  Note to my self: Good to name the element parameter in context
-    //  Here we are interating in an array of books so it is good to call the element 'book' :)
     myLibrary.forEach(book => {
-
-        // Creates a 'tr' element for the current book
         let tr = document.createElement('tr');
-
-        // After recreating the "God I wished there was an easier way to do this" meme 
-        // by asigning each 'td' manually I asked ChatGPT if *there was a easier/cleaner way to do this* 
-        // well turns out I'm dummer than the AI and there was a better way to do this
-        // I'll use this method in the future (if I don't forget :P)
 
         ['title', 'author', 'pages', 'read'].forEach(key => {
             let td = document.createElement('td');
@@ -52,6 +40,16 @@ function displayBook() {
 }
 
 
+newBook.addEventListener('mousedown', () => { 
+    addBookToLibrary('The Great Gatsby', 'F. Scott Fitzgerald', 180, 'Unread'); 
+});
+
+
+let lordOfTheRings  = new Book('Lord of The Rings', 'J.R.R. Tolkien', 295, 'Reading');
+let lightingThief   = new Book('Percy Jackson and the Lighting Thief', 'Rick Riordan', 100, 'Read');
+
+myLibrary[0] = lordOfTheRings;
+myLibrary[1] = lightingThief;
+
 
 displayBook();
-
