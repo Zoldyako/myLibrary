c07d752ada0659324fc28b5a93c24c20da6a181f
José Victor <zoldyakopersonal@gmail.com>
2024-10-03 17:20:40 -0300

    Finished

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ const myLibrary = [];
 const tbody     = document.querySelector('tbody');
 const newBook   = document.querySelector('button');
 const bookForm  = document.querySelector('.bookForm');
-const dialog    = document.querySelector('dialog');
-
+const dialogNewBook = document.querySelector('.newBook'); // Diálogo para novo livro
+const dialogEditStatus = document.querySelector('.edit'); // Diálogo para editar status
+const editForm = document.querySelector('#editForm'); // Formulário de edição de status
+let currentEditIndex = null; // Para armazenar o índice do livro sendo editado
 
 function Book(title, author, pages, status) {
     this.title  = title,
@@ -17,52 +19,52 @@ Book.prototype.bookInfo = function() {
 }
 
 Book.prototype.changeStatus = function(status) {
-    return this.status = status;
+    this.status = status;
 }
+
 
 function addBook(title, author, pages, status) {
     myLibrary.push(new Book(title, author, pages, status));
     displayBooks();
 }
 
+
 function deletBook(index) {
     myLibrary.splice(index, 1);
+    displayBooks();
 }
 
 
-function createDeletBtn(tr, index) {
+function createDeletBtn(index) {
     let deletBtn = document.createElement('button');
-    deletBtn.innerText = 'Delet';
+    deletBtn.innerText = 'Delete';
 
     deletBtn.addEventListener('click', () => {
-        let book = myLibrary[index];
         deletBook(index);
-        tr.remove();
     });
 
     return deletBtn;
 }
 
 
-function createReadStatusBtn(tr, index, status) {
+function createReadStatusBtn(index) {
     let readStatusBtn = document.createElement('button');
-    readStatusBtn.innerText = 'Edit';
+    readStatusBtn.innerText = 'Edit Status';
 
     readStatusBtn.addEventListener('click', () => {
-        let bookToChange = tr.dataset.index = index;
-        changeReadStatus(bookToChange, status);
-        alert("aa");
+        currentEditIndex = index; // Armazena o índice do livro a ser editado
+        openEditStatusDialog(index); // Abre o diálogo de edição
     });
 
     return readStatusBtn;
 }
 
-
-function changeReadStatus(book, status) {
-    book.bookStatus = status;
-    displayBooks();
+function openEditStatusDialog(index) {
+    const book = myLibrary[index];
+    // Selecionar o status atual no diálogo de edição
+    document.querySelector(`input[name="editStatus"][value="${book.status}"]`).checked = true;
+    dialogEditStatus.showModal();
 }
-
 
 function displayBooks() {
     tbody.innerHTML = '';
@@ -72,29 +74,25 @@ function displayBooks() {
 
         ['title', 'author', 'pages', 'status'].forEach(key => {
             let td = document.createElement('td');
-
             td.innerText = book[key];
             tr.appendChild(td);
         });
 
         let td = document.createElement('td');
-
-        td.appendChild(createReadStatusBtn(tr, index));
-        td.appendChild(createDeletBtn(tr, index));
+        td.appendChild(createReadStatusBtn(index));
+        td.appendChild(createDeletBtn(index));
         tr.appendChild(td);
 
-
-        tr.dataset.index = index
-        
         tbody.appendChild(tr);
     }); 
 }
 
-
-newBook.addEventListener('click', () => { dialog.showModal(); });
+// Para adicionar novos livros
+newBook.addEventListener('click', () => { 
+    dialogNewBook.showModal(); 
+});
 
 bookForm.addEventListener('submit', event => {
-
     event.preventDefault();
 
     const title = document.querySelector('#title').value;
@@ -105,11 +103,24 @@ bookForm.addEventListener('submit', event => {
     addBook(title, author, pages, status);
 
     bookForm.reset();
-    dialog.close();
+    dialogNewBook.close();
 });
 
+// Formulário para editar o status de leitura do livro
+editForm.addEventListener('submit', event => {
+    event.preventDefault();
 
+    const newStatus = document.querySelector('input[name="editStatus"]:checked').value;
+    
+    if (currentEditIndex !== null) {
+        myLibrary[currentEditIndex].changeStatus(newStatus); // Atualiza o status do livro
+        displayBooks(); // Recarrega a tabela com o novo status
+    }
 
+    dialogEditStatus.close(); // Fecha o diálogo após a atualização
+});
+
+// Livros pré-existentes
 let lordOfTheRings  = new Book('Lord of The Rings', 'J.R.R. Tolkien', 295, 'Reading');
 let lightingThief   = new Book('Percy Jackson and the Lighting Thief', 'Rick Riordan', 100, 'Read');
 
